78c9e5d4af0350bea74156ad5515f7185e870aff
Nebojsa Kamber <nebojsa.kamber@4zida.rs>
2021-04-15 14:32:14 +0200

    Fields in successful response

--- src/ZohoOAuthResponse.php
+++ src/ZohoOAuthResponse.php
@@ -2,6 +2,8 @@
 
 namespace Nebkam\ZohoOAuth;
 
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
 /**
  * @see https://www.zoho.com/crm/developer/docs/api/v2.1/access-refresh.html
  */
@@ -26,4 +28,29 @@ class ZohoOAuthResponse
 	 * @see ERROR_INVALID_CODE
 	 */
 	public ?string $error = null;
+
+	/**
+	 * @SerializedName("access_token")
+	 */
+	public ?string $accessToken;
+
+	/**
+	 * @SerializedName("refresh_token")
+	 */
+	public ?string $refreshToken;
+
+	/**
+	 * @SerializedName("expires_in")
+	 */
+	public ?int $expiresInSeconds;
+
+	/**
+	 * @SerializedName("api_domain")
+	 */
+	public ?string $apiDomain;
+
+	/**
+	 * @SerializedName("token_type")
+	 */
+	public ?string $tokenType;
 	}
