713b532e998f76ffcf38dc9b130275affbc875b1
Nebojsa Kamber <nebojsa.kamber@4zida.rs>
2021-06-07 10:50:55 +0200

    Fixes #1

--- tests/ZohoAuthServiceTest.php
+++ tests/ZohoAuthServiceTest.php
@@ -2,17 +2,15 @@
 
 namespace Tests;
 
-use DateTimeZone;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Nebkam\ZohoOAuth\ZohoOAuthService;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\NativeHttpClient;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -29,14 +27,10 @@ class ZohoAuthServiceTest extends TestCase
 			null,
 			new PhpDocExtractor()
 		);
-		$dateTimeNormalizer     = new DateTimeNormalizer([
-			DateTimeNormalizer::FORMAT_KEY   => 'd.m.Y. H:i',
-			DateTimeNormalizer::TIMEZONE_KEY => new DateTimeZone('Europe/Belgrade')
-		]);
 
 		return new Serializer(
-			[$dateTimeNormalizer, $objectNormalizer, new ArrayDenormalizer()],
-			[new NotNullJsonEncoder()]
+			[$objectNormalizer],
+			[new JsonEncoder()]
 		);
 		}
 
